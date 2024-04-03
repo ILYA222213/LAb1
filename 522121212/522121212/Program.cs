5aebfb26c0d349fe7735645723dca15399ccef07
Сом <117897751+ILYA222213@users.noreply.github.com>
2024-04-03 11:10:40 +0300

    Add files via upload

---
+++ 522121212/522121212/Program.cs
@@ -0,0 +1,16 @@
+﻿double n = Convert.ToDouble(Console.ReadLine());
+double x = Convert.ToDouble(Console.ReadLine());
+if (Math.Abs(x) > 1)
+{
+    double result = 0;
+    for (double i = 0; i <= n; i++)
+    {
+        result += 1 / ((2 * i + 1) * Math.Pow(x, 2 * i + 1));
+        Console.WriteLine(result);
+    }
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("х должно быть меньше 1");
+}
